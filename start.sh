3bbe8f091205144caeca16a3c11a1137075543a3
blreay <happyblreay@hotmail.com>
2021-09-23 11:13:41 +0800

    bashttpd: add check for tree utility

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 
 port=${1:-38080}
 
+if ! type tree >/dev/null 2>&1; then
+  echo "tree is not installed. please install tree: yum install -y tree"
+fi
+
 typeset CMD="ncat -4lkp ${port} -e ./bashttpd"
 echo "${CMD}"
 eval ${CMD}
