62edd99f0f3d558448839f028efe602be6d0e9a1
blreay <happyblreay@hotmail.com>
2024-04-07 10:44:26 +0800

    add daemon mode for start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,28 @@
 
 # nohup start.sh >/dev/null 2>&1 &
 
-port=${1:-38080}
+typeset daemon=0
+
+unset OPTIND
+while getopts :d ch; do
+  case $ch in
+    d) daemon=1;;
+  esac
+done
+shift $((OPTIND-1))
+
+typeset port=${1:-38080}
+echo "port=${port} daemon=${daemon}"
 
 if ! type tree >/dev/null 2>&1; then
   echo "tree is not installed. please install tree: yum install -y tree"
 fi
 
-typeset CMD="ncat -4lkp ${port} -e ./bashttpd"
-echo "${CMD}"
-eval ${CMD}
+if [[ ${daemon} -eq 1 ]]; then
+  nohup ./start.sh ${port} > /dev/null 2>&1 &
+else
+  typeset CMD="ncat -4lkp ${port} -e ./bashttpd"
+  echo "${CMD}"
+  eval ${CMD}
+fi
 
